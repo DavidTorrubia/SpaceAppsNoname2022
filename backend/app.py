9b25185cc545cb421f498f3ffedd1c750e298722
Holewalker <dani75esp@gmail.com>
2022-10-01 16:04:36 +0200

    ExtractText function

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+import extractText
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+
+    text = extractText.extracttext('..\..\sample.pdf')
+
+    return text
+
+
+# return text
 
 
 if __name__ == '__main__':

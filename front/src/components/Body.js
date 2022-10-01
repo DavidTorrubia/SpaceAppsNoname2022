9f10ad0f216a39edcb73fead7e81d838fe9a132f
Sami <fracasami@gmail.com>
2022-10-01 16:03:00 +0200

    Added Landing page front

---
+++ front/src/components/Body.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../style/Main.css";
+import BgVideo from "../video/bgVideo.mp4";
+export default function Body() {
+  return (
+    <body>
+      <div class="s01">
+        <video autoPlay muted loop id="myVideo">
+          <source src={BgVideo} type="video/mp4" />
+        </video>
+        <form className="z-index">
+          <fieldset>
+            <legend>Discover the Amazing City</legend>
+          </fieldset>
+          <div class="inner-form">
+            <div class="input-field first-wrap">
+              <input
+                id="search"
+                type="text"
+                placeholder="What are you looking for?"
+              />
+            </div>
+            <div class="input-field third-wrap">
+              <button class="btn-search" type="button">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </body>
+  );
+}

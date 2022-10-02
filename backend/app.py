cf2b12980f460c57c0ebe2212054258ab2bc03d6
James Miller <james.miller@trilogy.com>
2022-10-02 02:04:43 +0200

    change to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from urllib import response
 from flask import Flask
-from backend.searchScript import searchCoincidences
+from searchScript import searchCoincidences
 from flask import jsonify
 from flask import request
 import extractText
@@ -11,9 +11,9 @@ app = Flask(__name__)
 @app.route('/api/search', methods=['POST'])
 def search():
     content = request.get_json()
-    usertoken = searchScript.extractToken(param)
-    coincidences = searchScript.searchCoincidences(usertoken)
-    return jsonify(coincidences)
+    # usertoken = searchScript.extractToken(param)
+    # coincidences = searchScript.searchCoincidences(usertoken)
+    return jsonify(content)
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -21,8 +21,7 @@ def hello_world():  # put application's code here
     kw = extractText.getkeywords(text)
     return kw
 
-
-app.route('/api/v1/search/<param>', methods=['GET'])
+@app.route('/api/v1/search/<param>', methods=['GET'])
 def extracttoken(param):
     usertoken = searchScript.extractToken(param)
     coincidences = searchScript.searchCoincidences(usertoken)
